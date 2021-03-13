712a458c49e2df6f8577f18ee050aa8d9f5ae981
Matteo <chiacchius@air-di-matteo.fritz.box>
2021-03-13 11:52:21 +0100

    second commit

--- git_test/src/GitTest.java
+++ git_test/src/GitTest.java
@@ -3,7 +3,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int nuovo //ho aggiunto questa linea di codice
 		System.out.println("ciao baby");
 	}
 
